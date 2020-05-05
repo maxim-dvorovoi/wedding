854670b44f8bb99725e478e7f341b1c2357b359e
Maxim <vmolo4nik@gmail.com>
2020-05-05 21:39:19 +0300

    fix local api

--- src/mixin/computed.js
+++ src/mixin/computed.js
@@ -7,7 +7,7 @@ export default {
     },
     apiUrl() {
         if (process.env.NODE_ENV === 'development') {
-            return 'http://localhost/projects/seo_dashboard/dist/api.php';
+            return 'https://localhost/projects/seo_dashboard/dist/api.php';
         } else {
             return 'https://seo-admin.herokuapp.com/api.php';
         }
