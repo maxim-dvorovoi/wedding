14bd0a679c94a07b13e08049ed7a660f955ac88c
Maxim <vmolo4nik@gmail.com>
2020-05-05 13:00:23 +0300

    heroku test

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path')
 
 const app = express()
 
-//here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 // this * route is to serve project on different page routes except root `/`
